2d824ca657cd6af90e0fb39a33837a16f4b5ebbe
Daniel Dereniowski <leinad885@o2.pl>
2021-02-23 17:28:45 +0100

    Connected to database

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env"});
+
+const database = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+database.connect( (error) => {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log("MySQL connected");
+    }
+});
 
 app.listen(5000, () => {
     console.log("Server running on port 5000");
